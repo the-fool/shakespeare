4753b5c2c4156bd001d342defa52bd9acf605e37
Thomas Ruble <sketchbang@gmail.com>
2018-11-10 17:34:52 -0800

    basic csv ingester

--- ShakespeareAPI/Models/CsvIngester.cs
+++ ShakespeareAPI/Models/CsvIngester.cs
@@ -1,7 +1,32 @@
-namespace ShakespeareAPI.Models
-{
-    public class CsvIngester
-    {
-        
+using System;
+using System.Collections.Generic;
+using System.IO;
+using CsvHelper;
+namespace ShakespeareAPI.Models {
+    public class CsvIngester {
+        private class ShakespeareRow {
+            public string Dataline { get; set; }
+            public string Play { get; set; }
+            public string PlayerLine { get; set; }
+            public string ActSceneLine { get; set; }
+            public string Player { get; set; }
+            public string Text { get; set; }
+            public override string ToString () {
+                return base.ToString () + ": " + Play + ": " + Dataline;
+            }
+        }
+
+        public static void Ingest (ApplicationDbContext context) {
+
+            List<string> result = new List<string> ();
+
+            using (TextReader fileReader = File.OpenText ("models/Shakespeare_data.csv")) {
+                var csv = new CsvReader (fileReader);
+                csv.Read ();
+                var record = csv.GetRecord<ShakespeareRow> ();
+                Console.WriteLine (record.ToString());
+            }
+        }
+
     }
 }
--- ShakespeareAPI/Program.cs
+++ ShakespeareAPI/Program.cs
@@ -10,35 +10,45 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using ShakespeareAPI.Models;
 
-namespace ShakespeareAPI
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = CreateWebHostBuilder(args).Build();
+namespace ShakespeareAPI {
+    public class Program {
+        public static int Main (string[] args) {
+            foreach (var arg in args) {
+                Console.WriteLine (arg);
+            }
+            var host = CreateWebHostBuilder (args).Build ();
 
-            using (var scope = host.Services.CreateScope())
-            {
+            using (var scope = host.Services.CreateScope ()) {
                 var services = scope.ServiceProvider;
 
-                try
-                {
-                    var context = services.GetRequiredService<ApplicationDbContext>();
-                    DbInitializer.Initialize(context);
+                var context = services.GetRequiredService<ApplicationDbContext> ();
+                var logger = services.GetRequiredService<ILogger<Program>> ();
+
+                try {
+                    DbInitializer.Initialize (context);
+                } catch (Exception ex) {
+                    logger.LogError (ex, "An error occurred creating the DB.");
                 }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred creating the DB.");
+
+                if (args.Length > 0) {
+                    if (args[0] == "csv") {
+                        try {
+                            CsvIngester.Ingest (context);
+                            return 0;
+                        } catch (Exception ex) {
+                            logger.LogError (ex, "An error occurred ingesting CSV");
+                            return 1;
+                        }
+                    }
                 }
             }
 
-            host.Run();
+            host.Run ();
+            return 1;
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder (string[] args) =>
+            WebHost.CreateDefaultBuilder (args)
+            .UseStartup<Startup> ();
     }
 }
