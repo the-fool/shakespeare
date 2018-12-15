de6a2f576f4bccb85b8534daf80fd793848d52c2
Thomas Ruble <sketchbang@gmail.com>
2018-12-15 14:53:44 -0800

    add line migration

---
+++ ShakespeareAPI/Migrations/20181215224533_LineModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShakespeareAPI.Migrations
+{
+    public partial class LineModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Character",
+                table: "Lines",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CharacterLineNumber",
+                table: "Lines",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Character",
+                table: "Lines");
+
+            migrationBuilder.DropColumn(
+                name: "CharacterLineNumber",
+                table: "Lines");
+        }
+    }
+}
--- ShakespeareAPI/Models/Line.cs
+++ ShakespeareAPI/Models/Line.cs
@@ -7,6 +7,8 @@ namespace ShakespeareAPI.Models
         public int PlayID { get; set; }
         public Play Play { get; set; }
 
+        public string Character { get; set; }
+        public int CharacterLineNumber { get; set; }
         public int LineNumber { get; set; }
         public int ActNumber { get; set; }
         public int SceneNumber { get; set; }
