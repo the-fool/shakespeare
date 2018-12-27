be06653918087445ad4ed378f74391b658d19dba
Thomas Ruble <sketchbang@gmail.com>
2018-12-26 23:13:20 -0800

    set up console project

---
+++ backend/ShakespeareAPI.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ShakespeareAPI.Models;
+
+namespace ShakespeareAPI.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
